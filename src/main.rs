8caef7a4b5362c3bb29b306383a44c265c0fa105
shift-d <shiftkeydeveloper@gmail.com>
2021-12-14 19:02:19 +0300

    chore(tui): initial

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,31 @@
+use std::io;
+use tui::backend::CrosstermBackend;
+use tui::layout::{Constraint, Direction, Layout};
+use tui::widgets::{Block, Borders, Paragraph};
+use tui::Terminal;
 
-
-fn main() {
-    
+fn main() -> Result<(), io::Error> {
+    let stdout = io::stdout();
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+    terminal.draw(|f| {
+        let chunks = Layout::default()
+            .direction(Direction::Vertical)
+            .margin(1)
+            .constraints(
+                [
+                    Constraint::Percentage(10),
+                    Constraint::Percentage(80),
+                    Constraint::Percentage(10),
+                ]
+                .as_ref(),
+            )
+            .split(f.size());
+        let block = Block::default().title("Block").borders(Borders::ALL);
+        let paragraph = Paragraph::new("Hello, NeoSH!").block(block);
+        f.render_widget(paragraph, chunks[0]);
+        let block = Block::default().title("Block 2").borders(Borders::ALL);
+        f.render_widget(block, chunks[1]);
+    })?;
+    Ok(())
 }
