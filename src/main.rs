9e86101d09ccf283f73d47ebe97ff9622b1512af
shift-d <shiftkeydeveloper@gmail.com>
2021-12-19 12:11:35 +0300

    feat(render): prompt

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use std::io;
+use std::env;
+use std::io::{self, Stdout};
 use tui::backend::CrosstermBackend;
 use tui::layout::{Constraint, Direction, Layout};
 use tui::widgets::{Block, Borders, Paragraph};
@@ -8,20 +9,48 @@ fn main() -> Result<(), io::Error> {
     let stdout = io::stdout();
     let backend = CrosstermBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
+
+    let username = whoami::username();
+    let hostname = whoami::hostname();
+    let prompt = prompt(username, hostname);
+
+    terminal.clear()?;
+
+    render(&mut terminal, &prompt)?;
+
+    Ok(())
+}
+
+fn render(terminal: &mut Terminal<CrosstermBackend<Stdout>>, text: &str) -> Result<(), io::Error> {
     terminal.draw(|f| {
         let chunks = Layout::default()
             .direction(Direction::Vertical)
             .margin(1)
-            .constraints(
-                [
-                    Constraint::Percentage(100),
-                ]
-                .as_ref(),
-            )
+            .constraints([Constraint::Percentage(100)].as_ref())
             .split(f.size());
         let block = Block::default().title("NeoSH").borders(Borders::ALL);
-        let paragraph = Paragraph::new("Hello, NeoSH!").block(block);
+        let paragraph = Paragraph::new(text).block(block);
         f.render_widget(paragraph, chunks[0]);
     })?;
     Ok(())
+}
+
+fn prompt(username: String, hostname: String) -> String {
+    let cwd = env::current_dir()
+        .unwrap()
+        .into_os_string()
+        .into_string()
+        .unwrap();
+
+    let cwd = cwd.replace(&{
+        #[cfg(not(target_os = "windows"))]
+        let homedir = env::var("HOME").unwrap();
+
+        #[cfg(target_os = "windows")]
+        let homedir = env::var("USERPROFILE").unwrap();
+
+        homedir
+    }, "~");
+
+    format!("{}@{} {} > ", username, hostname, cwd)
 }
